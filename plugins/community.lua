bb6e281bb192af1f4d707f611e1381d0b3fba3db
Jari Van Melckebeke <jarivanmelckebeke@gmail.com>
2023-10-27 15:53:45 +0200

    added some plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,52 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+ { import = "astrocommunity.colorscheme.catppuccin" },
+ {
+  "catppuccin",
+  opts = {
+   integrations = {
+    sandwich = false,
+    noice = true,
+    mini = true,
+    leap = true,
+    markdown = true,
+    neotest = true,
+    cmp = true,
+    overseer = true,
+    lsp_trouble = true,
+    rainbow_delimiters = true,
+   }
+  }
+ },
+ { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+
+ { import = "astrocommunity.split-and-window.windows-nvim" },
+ {
+  "anuvyklack/windows.nvim",
+  lazy = false,
+  requires = {
+   "anuvyklack/middleclass",
+   "anuvyklack/animation.nvim",
+  },
+  config = function()
+   vim.o.winwidth = 10
+   vim.o.winminwidth = 10
+   vim.o.equalalways = false
+   require("windows").setup({
+    autowidth = {
+     enable = true,
+     winwidth = 10,
+     filetype = {
+      help = 2
+     }
+    },
+    ignore = {
+     buftype = {"quickfix"},
+     filetype = {"NvimTree", "neo-tree", "undotree", "gundo"},
+    }
+   })
+  end
+ }
 }
